5f8035be5b72ed12749f2fda32e9081cb5e5de33
Michel Maillard <info@maillard.li>
2024-03-14 18:21:48 +0100

    New folder structure & many improvements

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,14 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addWatchTarget("./src/sass/");
-    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addWatchTarget("./sass/");
+    eleventyConfig.addPassthroughCopy("./assets");
     return {
         dir: {
-            input: "src", // Set the source for 11ty
-            output: "_site", // This is the default
-            includes: "_includes", // All UI partials
-            layouts: "_layouts" // Base page layouts
-        }
-    }
-}
+            input: "content", // Set the source for 11ty
+            layouts: "../_layouts", // Base page layouts
+            includes: "../_includes", // All UI partials
+            data: "../_data", // JSON datasets
+            output: "_site" // This is the default
+        },
+        templateFormats: ["html", "md", "liquid"]
+    };
+};
